3d55e41e1c3d31e636d8601b25ddc733791a8fef
Sagar <sag333ar@gmail.com>
2026-06-29 16:31:50 +0530

    2 more exercise

---
+++ 07_string_ops2.py
@@ -0,0 +1,21 @@
+helloW = "Hello world!"
+
+# ----------- to Upper case
+
+print(helloW.upper())
+
+# ----------- to Lower case
+
+print(helloW.lower())
+
+# ----------- Is it starting with "Hello"?
+
+print(helloW.startswith("Hello"))
+
+# ----------- Is it starting with "World"?
+
+print(helloW.endswith("World"))
+
+# ----------- split by spaces
+
+print(helloW.split(" "))
---
+++ 08_conditions.py
@@ -0,0 +1,20 @@
+name = "Aditya"
+age = 22
+
+# ---- usage of  "and"
+
+if name == "Aditya" and age == 23:
+    print("Your name is Aditya, and you are also 22 years old.")
+
+# ---- usage of  "or"
+
+if name == "Aditya" or name == "Advaya":
+    print("Your name is either Aditya or Advaya.")
+    
+# ---- usage of  "in"
+    
+name = "Aditya"
+if name in ["Aditya", "Advaya", "Harsh", "Harshul"]:
+    print("Your name is either Aditya or Advay or ....")
+
+# - exercise - Grade - A, B, C, D
