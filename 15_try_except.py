2e69ee1a80024e544a4425a334fb7c472f9e48e2
Sagar <sag333ar@gmail.com>
2026-07-06 18:57:35 +0530

    recorded video for day 6

---
+++ 15_try_except.py
@@ -0,0 +1,10 @@
+try:
+  number = 10
+  value = 0
+  if number / value > 1:
+    print("greater than 1")
+  else:
+    print("not greater than 1")
+
+except:
+  print("Something weng wrong")
---
+++ 16_built_in_functions.py
@@ -0,0 +1,24 @@
+print("print function")
+
+print(abs(-30))
+
+print(len('Hello World!'))
+
+print(bool(0))
+print(bool(1))
+print(bool(100))
+print(bool('Hello'))
+print(bool(None))
+
+print(dir('Hello'))
+list = [1, 3, 4]
+print(dir(list))
+# print(help(list.pop))
+
+mycode = "print('Hello World!')"
+eval(mycode)
+exec(mycode)
+
+print(10 + int("20"))
+print("Hello " + str(30))
+print("Hello ", (float("1234.34") + float("22.23")))
