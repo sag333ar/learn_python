6625d61e35187ffba9f46218e0cc2e31a367004f
Sagar <sag333ar@gmail.com>
2026-07-28 15:47:44 +0530

    file handling & using PIP package manager

--- 17_files.py
+++ 17_files.py
@@ -6,45 +6,50 @@ if os.path.exists('myfile.txt') == False:
   sys.exit()
 
 myFileVariable = open('myfile.txt', 'r')
-print(myFileVariable.read())
+filecontent = myFileVariable.read()
+print(filecontent)
 myFileVariable.close()
 
-# - read whole file
+# # - read whole file
 
 with open('myfile.txt', 'r') as file:
   print(file.read())
 
 print("File is closed without close function")
 
-# - read first 8 bytes of file
+# # - read first 8 bytes of file
 
 with open('myfile.txt', 'r') as file:
   print(file.read(8))
 
-print("File is closed without close function")
+# print("File is closed without close function")
   
-# - read file lines
+# # - read file lines
 with open('myfile.txt', 'r') as file:
+  print("going to read first line")
   print(file.readline())
+  print("going to read second line")
   print(file.readline())
+  print("going to read third line")
   print(file.readline())
+  print("going to close the file")
   
-print("File is closed without close function")
+# print("File is closed without close function")
 
-# - append one line to existing file
+# # - append one line to existing file
 
 with open('myfile.txt', 'a') as file:
   file.write("\nThis is forth line.")
   
-with open('myfile.txt', 'r') as file:
-  print(file.read())
+# with open('myfile.txt', 'r') as file:
+#   print(file.read())
   
-# - append one line to existing file
+# # - append one line to existing file
 
-with open('myfile.txt', 'w') as file:
-  file.write("I am going to re-write everything")
+# with open('myfile.txt', 'w') as file:
+#   file.write("I am going to re-write everything")
   
-with open('myfile.txt', 'r') as file:
-  print(file.read())
+# with open('myfile.txt', 'r') as file:
+#   print(file.read())
 
-os.remove('myfile.txt')
+# os.remove('myfile.txt')
